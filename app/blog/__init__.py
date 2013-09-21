2c88b9f204283ebc52530709106e34f40c6f86cf
Andrew Lim <andrew.b.lim@gmail.com>
2013-09-21 09:43:10 -0400

    Set up test blog post

--- app/blog/__init__.py
+++ app/blog/__init__.py
@@ -1,6 +1,7 @@
 import cherrypy
 import jinja2
 
+# specify a base_dir...
 env = jinja2.Environment(loader=jinja2.FileSystemLoader('templates'))
 
 class Blog(object):
@@ -8,3 +9,9 @@ class Blog(object):
     @cherrypy.expose
     def index(self):
         return env.get_template('blog.jinja2').render()
+
+    # temporary, obviously not going to write this for every post
+    @cherrypy.expose
+    def testpost(self):
+        post = open('blog/content/test-post.html').read()
+        return env.get_template('blogpost.jinja2').render(post=post)
