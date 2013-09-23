5f6d7a6513b3778b162e3b6f1f65cefe80b9928a
Andrew Lim <andrew.b.lim@gmail.com>
2013-09-22 23:21:57 -0400

    Added basic login template, renmamed .jinja2 to .html

--- andywrote/andywrote.py
+++ andywrote/andywrote.py
@@ -11,6 +11,23 @@ SECRET_KEY = 'development_key'
 
 SQLALCHEMY_DATABASE_URI = "postgresql://andywrote@localhost/andywrote"
 
+## Flask-Security
+
+SECURITY_LOGIN_USER_TEMPLATE = "security/login_user.html"
+
+SECURITY_TRACKABLE     = True
+SECURITY_PASSWORD_HASH = 'bcrypt'
+SECURITY_PASSWORD_SALT = 'development_salt'
+
+# all these flags are set to False, their defaults in Flask-Security, 
+# but might be useful if you ever want to make this a multi-user site
+
+SECURITY_CONFIRMABLE  = False
+SECURITY_REGISTERABLE = False
+SECURITY_RECOVERABLE  = False
+SECURITY_CHANGEABLE   = False
+
+
 app = Flask(__name__)
 app.config.from_object(__name__)
 
@@ -21,15 +38,27 @@ roles_users = db.Table('roles_users',
                        db.Column('role_id', db.Integer, db.ForeignKey('role.id')))
 
 class Role(db.Model, RoleMixin):
+
     id          = db.Column(db.Integer, primary_key=True)
     name        = db.Column(db.String(80), unique=True)
     description = db.Column(db.String(255))
 
 class User(db.Model, UserMixin):
-    id       = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(40))
-    name     = db.Column(db.String(80))
-    email    = db.Column(db.String(120), unique=True)
+
+    id           = db.Column(db.Integer, primary_key=True)
+    name         = db.Column(db.String(80))
+    email        = db.Column(db.String(255), unique=True)
+    password     = db.Column(db.String(255))
+    active       = db.Column(db.Boolean)
+
+    last_login_at    = db.Column(db.DateTime)
+    current_login_at = db.Column(db.DateTime)
+    last_login_ip    = db.Column(db.String(40))
+    current_login_ip = db.Column(db.String(40))
+    login_count      = db.Column(db.Integer)
+
+    roles        = db.relationship('Role', secondary=roles_users, 
+                                   backref=db.backref('users', lazy='dynamic'))
 
 # Set up Flask-Security
 user_datastore = SQLAlchemyUserDatastore(db, User, Role)
@@ -37,7 +66,7 @@ security = Security(app, user_datastore)
 
 @app.route('/')
 def about():
-    return render_template('about.jinja2')
+    return render_template('about.html')
 
 if __name__ == '__main__':
     app.run()
--- serve.py
+++ serve.py
@@ -3,21 +3,3 @@ from andywrote import app
 
 if __name__ == '__main__':
     app.run()
-
-# from flask import Flask, render_template
-# app = Flask(__name__)
-
-# @app.route('/')
-# def index():
-#     return render_template('about.jinja2')
-
-# @app.route('/reading/')
-# def reading():
-#     return render_template('reading.jinja2')
-
-# @app.route('/blog/')
-# def blog():
-#     return render_template('blog.jinja2')
-
-# if __name__ == '__main__':
-#     app.run(debug=True)
